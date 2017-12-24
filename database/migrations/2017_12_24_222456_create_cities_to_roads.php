e120145cdbbc573d03dbd24aad115eb97a283c5b
Arnold.Z-PC <zhangxiangyun@ixiangyun.me>
2017-12-24 22:49:41 +0800

    add tables

---
+++ database/migrations/2017_12_24_222456_create_cities_to_roads.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesToRoads extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities_to_roads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('roadId');
+            $table->integer('cityId');
+            $table->timestamps();
+            // $table->foreign('roadId')->references('id')->on('roads')->onDelete('cascade');
+            // $table->foreign('cityId')->references('id')->on('cities')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('CitiesToRoads');
+    }
+}
