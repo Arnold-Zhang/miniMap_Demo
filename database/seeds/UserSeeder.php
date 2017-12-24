932e0474b4b5924c19c63481c434e748072bd8f1
Arnold.Z-PC <zhangxiangyun@ixiangyun.me>
2017-12-24 18:58:06 +0800

    set default admin user

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User();
+        $admin->name = "Admin";
+        $admin->email = "admin@test.com";
+        $admin->password = bcrypt("admin");
+        $admin->isAdmin = true;
+        $admin->save();
+    }
+}
