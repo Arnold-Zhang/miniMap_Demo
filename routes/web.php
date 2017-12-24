c85877cd4e0e405c9e0c5fed3c9a81df1f402b4a
Arnold.Z-PC <zhangxiangyun@ixiangyun.me>
2017-12-25 01:19:03 +0800

    set sign in up out routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,14 @@
 
 Route::get('/', 'StaticPagesController@home')->name('home');
 
+//显示注册页面
+Route::get('signup', 'UsersController@create')->name('signup');
+
 //用户资源路由
 Route::resource('users', 'UsersController');
 //显示登陆页面
-Route::get('login', 'SessionsController@create')->name('signIn');
+Route::get('signIn', 'SessionsController@create')->name('signIn');
+//创建新会话（登陆）
+Route::post('signIn', 'SessionsController@store')->name('signIn');
+//销毁会话（退出登录）
+Route::delete('signout', 'SessionsController@destroy')->name('signOut');
