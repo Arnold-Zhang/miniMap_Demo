0bbb5c9d39a5f9416d460b5147aebcb05c2c13a6
Abra.Zhang <zhangxiangyun@carkusoft.com>
2017-12-25 10:03:30 +0800

    add fillable for roads

--- app/Models/Road.php
+++ app/Models/Road.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Road extends Model
 {
+    protected $fillable = [
+        'distance',
+    ];
+
     public function cities()
     {
         return $this->belongsToMany('App\Models\City', 'CitiesToRoads', 'roadId', 'cityId');
