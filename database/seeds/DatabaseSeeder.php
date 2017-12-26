7e5e092059a23c7851d5dd1dbab8be21ffb0dee4
Abra.Zhang <zhangxiangyun@carkusoft.com>
2017-12-26 09:45:10 +0800

    fix seeder bug

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(CitySeeder::class);
-        $this->call(RoadSeeder::class);
+        // $this->call(RoadSeeder::class);  // 生成City数据之后手动运行RoadSeeder
     }
 }
