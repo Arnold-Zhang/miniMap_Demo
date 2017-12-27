eab9ec3c233483f075643d7f457deb12442c3b5c
Abra.Zhang <zhangxiangyun@carkusoft.com>
2017-12-27 16:45:11 +0800

    fix return arr bug when two cities unreachable

--- app/Models/Common.php
+++ app/Models/Common.php
@@ -19,8 +19,8 @@ class Common {
 
         // 若存在A或B没有道路,直接距离返回 '-'
         if (!in_array($idA, $citiesIds) || !in_array($idB, $citiesIds)) {
-            return ['shortests' => INF,
-                     'traces' => City::find($idA)->name . " to " . City::find($idB)->name
+            return ['shortests' => [$idB => INF],
+                     'traces' => [$idB =>City::find($idA)->name . " to " . City::find($idB)->name]
                 ];
         }
 
